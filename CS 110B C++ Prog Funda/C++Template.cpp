0cf8ce5bde62238374484076c4b639889fec40a9
William Sawyer <wsawyer42@gmail.com>
2022-07-09 11:14:44 -0700

    Add files via upload

---
+++ CS 110B C++ Prog Funda/C++Template.cpp
@@ -0,0 +1,26 @@
+/*
+William T Sawyer
+02/02/2020
+Class: CS 110B Comp Prog Fundamentals C++
+Instructor: Dave Harden
+Assignment ?
+
+Description: Compare month * date to two digit year from user input and see if true or false
+User input expected to be in 6/10/60 similar format
+
+How it works: 
+
+User Input:
+Program Output:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+}
+
+/*
+
+*/
